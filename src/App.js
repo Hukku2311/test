2051bd36a836c8aacc62eb7d3de74f1c7236fea8
Hukku2311 <hitechhtabkuh@gmail.com>
2020-09-07 07:46:38 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import "./index.css"
+ 
+function App(){
+  return(
+    <div className="grid-container">
+      <header>MISHAJ</header>
+    <main>dsahdia</main>
+    <footer><a href="https://www.mishaj.info">MISHAJ ADVERTISING</a></footer>
+    </div>
+  );
+}
+
+
+export default App;
